709acfb43590f81e008f9dd1a09156cca0c99a7e
othmane <otmanebnr80@gmail.com>
2025-10-08 10:18:23 +0100

    Task4 is done

---
+++ Task4.php
@@ -0,0 +1,22 @@
+<?php 
+    function Ispalindrome($Word){
+        $WorldLen = strlen($Word);
+        for($i = 0; $i < $WorldLen/2 ; $i++){
+            if($Word[$i] != $Word[$WorldLen-$i-1])
+                return false;
+        }
+        return true;
+    }
+    echo Ispalindrome("yahay") ? "is Palindrome" : "is not palindrome";
+?>
+
+
+<!-- 
+Bonus: make it case-insensitive :
+
+function IsPalindrome($word) {
+    $word = strtolower($word);
+    return strrev($word) === $word;
+}
+
+echo IsPalindrome("RaceCar") ? "is Palindrome" : "is not palindrome"; -->
