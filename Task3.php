80614a46d75013c941f2105e18e6ea42a393ee7f
othmane <otmanebnr80@gmail.com>
2025-10-08 09:31:10 +0100

    Task 3 & 4 are done

--- Task3.php
+++ Task3.php
@@ -1,4 +1,3 @@
-// my version  
 
 <?php
 function Factorial($Number){
@@ -12,44 +11,3 @@ function Factorial($Number){
 
 echo "Result " . Factorial(5); 
 ?>
-
-
-// version of chatgpt 
-// 1. Using recursion (function calls itself)
-
-<?php
-function Factorial($n) {
-    if ($n <= 1) return 1;
-    return $n * Factorial($n - 1);
-}
-
-echo "Result: " . Factorial(5);
-?>
-
-// 2. Using while loop
-
-<?php
-function Factorial($n) {
-    $result = 1;
-    while ($n > 1) {
-        $result *= $n;
-        $n--;
-    }
-    return $result;
-}
-
-echo "Result: " . Factorial(5);
-?>
-
-
-// 3. Using built-in functions (advanced)
-
-<?php
-function Factorial($n) {
-    return array_product(range(1, $n));
-}
-
-echo "Result: " . Factorial(5);
-?>
-
-
